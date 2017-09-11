47d363ab4db7e960c4ad6364afc0bf9bc3cd77df
as-is-prog <as-is-prog@outlook.jp>
2017-09-11 10:19:50 +0900

    プロジェクト ファイルを追加します。

---
+++ oi-s-lang/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oi_s_lang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
